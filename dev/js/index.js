31e68317e16d7e5f5144a6c8978a341caa368e34
ruru1225 <ailer8469@email.com>
2024-11-14 01:50:39 +0800

    11/13

--- dev/js/index.js
+++ dev/js/index.js
@@ -17,7 +17,6 @@ $(function(){
         $('.hamburger').removeClass('active');
         $('.nav_menu').removeClass('active');
     });
-
     // 點擊空白處關閉選單
     $(document).on('click',function(event) {
         if (!$(event.target).closest('.navbar').length) {
@@ -25,7 +24,6 @@ $(function(){
             $('.nav_menu').removeClass('active');
         }
     });
-
     // 添加滑過效果
     $('.nav_menu li a').on('hover',
         function() {
@@ -35,17 +33,14 @@ $(function(){
             $(this).css('color', 'white');
         }
     );
-
     // 滾動時固定導航欄（選用）
-    $(window).scroll(function() {
+    $(window).on('scroll',function() {
         if ($(this).scrollTop() > 50) {
             $('.navbar').addClass('fixed');
         } else {
             $('.navbar').removeClass('fixed');
         }
     });
-
-
     $('.center_carousel').slick({
         dots: true,
         infinite: true,
@@ -53,5 +48,92 @@ $(function(){
         fade: true,
         slide: 'div',
         cssEase: 'linear',
+    });
+
+    // 簡略內容
+    $('.class_text p').each(function() {
+        let text = $(this).text();
+        if (text.length > 40) {
+            $(this).text(text.substring(0, 40) + '...');
+        }
+    });
+
+    // 分頁內容
+    const $container = $('.project_container');
+    const $items = $container.find('.project_items');
+    const itemsPerPage = 9;
+    let currentPage = 1;
+    function showPage(page) {
+        $items.hide();
+        const start = (page - 1) * itemsPerPage;
+        const end = start + itemsPerPage;
+        $items.slice(start, end).show();
+
+        updatePagination();
+    }
+    function updatePagination() {
+        const totalItems = $items.length;
+        const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+        $('.page_numbers').empty();
+        for (let i = 1; i <= totalPages; i++) {
+            const $pageBtn = $('<button>').addClass('page_btn').text(i);
+            if (i === currentPage) {
+                $pageBtn.addClass('active');
+            }
+            $('.page_numbers').append($pageBtn);
+        }
+
+        $('.prev_page').prop('disabled', currentPage === 1);
+        $('.next_page').prop('disabled', currentPage === totalPages);
+    }
+    $('.prev_page').on('click',function() {
+        if (currentPage > 1) {
+            currentPage--;
+            showPage(currentPage);
+        }
+    });
+    $('.next_page').on('click',function() {
+        const totalItems = $items.length;
+        const totalPages = Math.ceil(totalItems / itemsPerPage);
+        if (currentPage < totalPages) {
+            currentPage++;
+            showPage(currentPage);
+        }
+    });
+    $(document).on('click', '.page_btn', function() {
+        currentPage = parseInt($(this).text());
+        showPage(currentPage);
+    });
+
+    showPage(1); // 顯示第一頁
+
+    // 往上滑
+    $('.scrollUp').on('click',function() {
+        $('html, body').animate({ scrollTop: 0 }, 600);
+    });
+    $(window).on('scroll',function() {
+        if ($(this).scrollTop() > 300) {
+            $('.scrollUp').fadeIn();
+        } else {
+            $('.scrollUp').fadeOut();
+        }
+    });
+
+    // 過濾器
+    const $filterItems = $('.title_class li');
+    const $projectItems = $('.project_items');
+
+    $filterItems.on('click',function() {
+        const selectedClass = $(this).attr('class');
+        if (selectedClass === 'all_class') {
+            // 顯示all
+            $projectItems.show();
+        } else {
+            // 隱藏
+            $projectItems.hide();
+            // 顯示屬於該類別的項目
+            $('.' + selectedClass).show();
+        }
     });
 });
---
+++ dev/js/project.js
@@ -0,0 +1,132 @@
+const projects = [
+    {
+        id: "021",
+        image: "assets/img/photo2_0000_list09.png",
+        tags: ["扁平化", "極簡","童趣", "白","紅", "綠"],
+        description: "太我国也游大问关尘老服德的是，见又了愚家不仃极云畴五罪于她一，郭不范，意恼你往慨她升下，们徒李，自月色否乐当败为守曰六五，自司他色。",
+        category: "flatten minimalist childlike"
+    },
+    {
+        id: "022",
+        image: "assets/img/photo2_0001_list08.png",
+        tags: ["極簡", "童趣","紅","黃"],
+        description: "太我国也游大问关尘老服德的是，见又了愚家不仃极云畴五罪于她一，郭不范，意恼你往慨她升下，们徒李，自月色否乐当败为守曰六五，自司他色。",
+        category: "minimalist childlike"
+    }
+    ,
+    {
+        id: "023",
+        image: "assets/img/photo2_0002_list07.png",
+        tags: ["復古", "質感","紅","紫"],
+        description: "太我国也游大问关尘老服德的是，见又了愚家不仃极云畴五罪于她一，郭不范，意恼你往慨她升下，们徒李，自月色否乐当败为守曰六五，自司他色。",
+        category: "retro texture"
+    }
+    ,
+    {
+        id: "024",
+        image: "assets/img/photo2_0003_list06.png",
+        tags: ["扁平化", "質感","橘","黑"],
+        description: "太我国也游大问关尘老服德的是，见又了愚家不仃极云畴五罪于她一，郭不范，意恼你往慨她升下，们徒李，自月色否乐当败为守曰六五，自司他色。",
+        category: "flatten texture"
+    }
+    ,
+    {
+        id: "025",
+        image: "assets/img/photo2_0004_list05.png",
+        tags: ["復古","極簡","紅","黃","白"],
+        description: "太我国也游大问关尘老服德的是，见又了愚家不仃极云畴五罪于她一，郭不范，意恼你往慨她升下，们徒李，自月色否乐当败为守曰六五，自司他色。",
+        category: "retro minimalist"
+    }
+    ,
+    {
+        id: "026",
+        image: "assets/img/photo2_0005_list04.png",
+        tags: ["華麗","童趣","紅","粉"],
+        description: "太我国也游大问关尘老服德的是，见又了愚家不仃极云畴五罪于她一，郭不范，意恼你往慨她升下，们徒李，自月色否乐当败为守曰六五，自司他色。",
+        category: "gorgeous childlike"
+    }
+    ,
+    {
+        id: "027",
+        image: "assets/img/photo2_0006_list03.png",
+        tags: ["扁平化","紅","粉"],
+        description: "太我国也游大问关尘老服德的是，见又了愚家不仃极云畴五罪于她一，郭不范，意恼你往慨她升下，们徒李，自月色否乐当败为守曰六五，自司他色。",
+        category: "flatten"
+    }
+    ,
+    {
+        id: "028",
+        image: "assets/img/photo2_0007_list02.png",
+        tags: ["華麗","復古","金","綠"],
+        description: "太我国也游大问关尘老服德的是，见又了愚家不仃极云畴五罪于她一，郭不范，意恼你往慨她升下，们徒李，自月色否乐当败为守曰六五，自司他色。",
+        category: "gorgeous retro"
+    }
+    ,
+    {
+        id: "029",
+        image: "assets/img/photo2_0008_list01.png",
+        tags: ["童趣","白","粉"],
+        description: "太我国也游大问关尘老服德的是，见又了愚家不仃极云畴五罪于她一，郭不范，意恼你往慨她升下，们徒李，自月色否乐当败为守曰六五，自司他色。",
+        category: "childlike"
+    }
+    ,
+    {
+        id: "030",
+        image: "assets/img/photo2_0008_list01.png",
+        tags: ["極簡", "質感","紅","綠"],
+        description: "太我国也游大问关尘老服德的是，见又了愚家不仃极云畴五罪于她一，郭不范，意恼你往慨她升下，们徒李，自月色否乐当败为守曰六五，自司他色。",
+        category: "minimalist texture"
+    }
+];
+
+
+$(function(){
+    const projectContainer = $(".project_container"); 
+        projects.forEach(project => {
+            // 創建每個項目的 HTML 結構
+            const projectItem = $(`
+                <div class="project_items ${project.category}">
+                    <div class="project_item">
+                        <div class="class_img">
+                            <img src="${project.image}" alt="">
+                            <div class="hover_text">
+                                <h1 class="class_id">${project.id}</h1>
+                                <ul class="item_class">
+                                    ${project.tags.map(tag => `<li>${tag}</li>`).join('')}
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="class_text">
+                            <h1 class="class_id">${project.id}</h1>
+                            <p>${project.description}</p>
+                        </div>
+                    </div>
+                </div>
+            `);
+
+            // 將生成的項目附加到容器
+            projectContainer.append(projectItem);
+        });
+
+        // 點擊窗
+        $(".project_item").on("click", function() {
+            // 取得該 .project_item 的圖片來源
+            const imgSrc = $(this).find("img").attr("src");
+            
+            // 設置模態框中的圖片
+            $("#modalImage").attr("src", imgSrc);
+            
+            // 顯示模態框
+            $("#modal").fadeIn(500);
+        });
+        
+        // 當點擊 .close 或模態框時關閉模態框
+        $(".close, #modal").on("click", function() {
+            $("#modal").fadeOut(500);
+        });
+        
+        // 避免點擊模態框內的圖片關閉模態框
+        $("#modalImage").on("click", function(event) {
+            event.stopPropagation();
+        });
+});
